06b31cca5e6b69e080df043afc8e45617e7e2429
mochandri <72057874+mochandri@users.noreply.github.com>
2023-03-17 03:15:45 +0700

    password

--- app/src/main/java/com/example/crudlsp/profileActivity.java
+++ app/src/main/java/com/example/crudlsp/profileActivity.java
@@ -1,5 +1,6 @@
 package com.example.crudlsp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -59,24 +60,23 @@ public class profileActivity extends AppCompatActivity {
         cPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View resetpasswordlayout= LayoutInflater.from(profileActivity.this).inflate(R.layout.change_password,null);
-                final EditText Oldpass = resetpasswordlayout.findViewById(R.id.edt_old_password);
-               final EditText Newpass = resetpasswordlayout.findViewById(R.id.edt_new_password);
-               final EditText Confirmpass = resetpasswordlayout.findViewById(R.id.edt_confirm_password);
+                View resetpasswordlayout = LayoutInflater.from(profileActivity.this).inflate(R.layout.change_password,null);
+                EditText OldPass = resetpasswordlayout.findViewById(R.id.edt_old_password);
+                EditText NewPass = resetpasswordlayout.findViewById(R.id.edt_new_password);
+                EditText ConformPass = resetpasswordlayout.findViewById(R.id.edt_conform_password);
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(profileActivity.this);
                 builder.setTitle("CHANGE PASSWORD");
                 builder.setView(resetpasswordlayout);
-                builder.setPositiveButton("CHANGE", new DialogInterface .OnClickListener() {
+                builder.setPositiveButton("CHANGE", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                       final String oldpassword = Oldpass.getText().toString().trim();
-                       final String newpassword = Newpass.getText().toString().trim();
-                       final String confirmpassword =Confirmpass.getText().toString().trim();
+                        String oldpassword = OldPass.getText().toString().trim();
+                        String newpassword = NewPass.getText().toString().trim();
+                        String conformpassword = ConformPass.getText().toString().trim();
 
-                        if(oldpassword.isEmpty()|| newpassword.isEmpty()|| confirmpassword.isEmpty()){
-                            massage("some feild are empty");
-
+                        if (oldpassword.isEmpty() || newpassword.isEmpty() || conformpassword.isEmpty()){
+                            massage("Terdapat Field Kosong");
                         }else {
                             progressDialog.show();
                             StringRequest stringRequest = new StringRequest(Request.Method.POST, Urls.RESET_PASSWORD_URL,
@@ -84,40 +84,99 @@ public class profileActivity extends AppCompatActivity {
                                         @Override
                                         public void onResponse(String response) {
                                             progressDialog.dismiss();
-                                              massage(response);
+                                            massage(response);
                                         }
                                     }, new Response.ErrorListener() {
                                 @Override
                                 public void onErrorResponse(VolleyError error) {
-                                    progressDialog.dismiss();
-                                    massage(error.getMessage());
+                                        progressDialog.dismiss();
+                                        massage(error.getMessage());
                                 }
                             }){
+                                @Nullable
                                 @Override
-                                public Map<String, String> getHeaders() throws AuthFailureError {
-                                   Map<String,String> params = new HashMap<>();
-                                   params.put("oldpassword",oldpassword);
-                                   params.put("newpassword",newpassword);
-                                   params.put("confirmpassword",confirmpassword);
-                                   params.put("email",mEmail);
-                                   return params;
+                                protected Map<String, String> getParams() throws AuthFailureError {
+                                    Map<String, String> params = new HashMap<>();
+                                    params.put("oldpassword", oldpassword);
+                                    params.put("newpassword",newpassword);
+                                    params.put("conformpassword", conformpassword);
+                                    params.put("email", mEmail);
+
+                                    return params;
                                 }
                             };
-
-                            RequestQueue queue= Volley.newRequestQueue(profileActivity.this);
+                            RequestQueue queue = Volley.newRequestQueue(profileActivity.this);
                             queue.add(stringRequest);
-
-
                         }
                     }
                 });
-
                 AlertDialog dialog = builder.create();
                 dialog.show();
-
-
             }
         });
+
+//        cPass.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                View resetpasswordlayout= LayoutInflater.from(profileActivity.this).inflate(R.layout.change_password,null);
+//                EditText Oldpass = resetpasswordlayout.findViewById(R.id.edt_old_password);
+//                EditText Newpass = resetpasswordlayout.findViewById(R.id.edt_new_password);
+//                EditText Conformpass = resetpasswordlayout.findViewById(R.id.edt_confirm_password);
+//
+//                AlertDialog.Builder builder = new AlertDialog.Builder(profileActivity.this);
+//                builder.setTitle("CHANGE PASSWORD");
+//                builder.setView(resetpasswordlayout);
+//                builder.setPositiveButton("CHANGE", new DialogInterface .OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                       String oldpassword = Oldpass.getText().toString().trim();
+//                       String newpassword = Newpass.getText().toString().trim();
+//                       String conformpassword =Conformpass.getText().toString().trim();
+//
+//                        if(oldpassword.isEmpty()|| newpassword.isEmpty()|| conformpassword.isEmpty()){
+//                            massage("some feild are empty");
+//
+//                        }else {
+//                            progressDialog.show();
+//                            StringRequest stringRequest = new StringRequest(Request.Method.POST, Urls.RESET_PASSWORD_URL,
+//                                    new Response.Listener<String>() {
+//                                        @Override
+//                                        public void onResponse(String response) {
+//                                            progressDialog.dismiss();
+//                                              massage(response);
+//                                        }
+//                                    }, new Response.ErrorListener() {
+//                                @Override
+//                                public void onErrorResponse(VolleyError error) {
+//                                    progressDialog.dismiss();
+//                                    massage(error.getMessage());
+//                                }
+//                            }){
+//                                @Override
+//                                public Map<String, String> getHeaders() throws AuthFailureError {
+//                                   Map<String,String> params = new HashMap<>();
+//                                   params.put("oldpassword",oldpassword);
+//                                   params.put("newpassword",newpassword);
+//                                   params.put("conformpassword",conformpassword);
+//                                   params.put("email",mEmail);
+//                                   return params;
+//                                }
+//                            };
+//
+//                            RequestQueue queue= Volley.newRequestQueue(profileActivity.this);
+//                            queue.add(stringRequest);
+//
+//
+//                        }
+//                    }
+//                });
+//
+//                AlertDialog dialog = builder.create();
+//                dialog.show();
+//
+//
+//            }
+//        });
 
     }
     public void massage(String massage){
